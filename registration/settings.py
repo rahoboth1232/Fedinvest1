4ff9abd5a2aaf4586c09aeb08df828b152cb02b7
Priy Gill <your-email@example.com>
2026-01-13 16:17:57 +0530

    change in settings

--- registration/settings.py
+++ registration/settings.py
@@ -85,7 +85,8 @@ WSGI_APPLICATION = 'registration.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/var/www/django_db/db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
